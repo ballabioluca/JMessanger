fe9aea762b57591e3c8f33db5fa635500e6c55fe
balabioluca <luca.ballabio@outlook.com>
2025-09-27 12:16:52 +0200

    Client --> Server (no res) + try/catch

---
+++ src/client.java
@@ -0,0 +1,22 @@
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.net.Socket;
+
+public class client {
+    public static void main(String[] args) {
+        try (Socket socket = new Socket("localhost", 4999);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in))) {
+
+            System.out.println("Scrivi un messaggio e premi INVIO:");
+            String linea = userInput.readLine();   // legge una riga da tastiera
+            out.println(linea);                   // invia la riga al server
+            System.out.println("Messaggio inviato: " + linea);
+
+        } catch (IOException e) {
+            System.out.println("Errore");
+        }
+    }
+}
---
+++ src/server.java
@@ -0,0 +1,26 @@
+import java.net.*;
+import java.io.*;
+
+public class server {
+    public static void main(String[] args) {
+        System.out.println("SERVER ONLINE");
+        try (ServerSocket ss = new ServerSocket(4999)) {
+            for (int i = 0; i < 10; i++) {
+                try (Socket s = ss.accept();
+                     BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                     PrintWriter out = new PrintWriter(s.getOutputStream(), true)) {
+
+                    System.out.println("CLIENT --> CONNECTED");
+
+                    String str = in.readLine();
+                    System.out.println("CLIENT: " + str);
+
+                    // Risposta al client
+                    out.println("INCOMING MSG: " + str);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
